d5d382399a732ab3ae553cb56184e4e4ac8ccf4b
trinp3 <81163831+trinp3@users.noreply.github.com>
2021-04-05 03:29:25 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(5)
+    Grapes.setPosition(0, randint(0, 50))
+    Grapes.vy = 50
+})
+let Grapes: Sprite = null
+scene.setBackgroundColor(7)
 let Basket = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -16,3 +23,22 @@ let Basket = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+controller.moveSprite(Basket, 100, 0)
+Grapes = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 4 e . . . . . . . . 
+    . . . . . . 4 e . . . . . . . . 
+    . . . . . . e 4 . . . . . . . . 
+    . . . . c c c e . . . . . . . . 
+    . . . . a a c c c c . . . . . . 
+    . . . . a a c c c c c . . . . . 
+    . . . . . c a a c c c . . . . . 
+    . . . . c c a a c c . . . . . . 
+    . . . . c a c c c c . . . . . . 
+    . . . . . a a c c . . . . . . . 
+    . . . . c c a a c c . . . . . . 
+    . . . . c c c a a c . . . . . . 
+    . . . . . c a c . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
