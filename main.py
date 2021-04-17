76fd402f3ff23bdb289c841661af67c52fe56d16
trinp3 <81163831+trinp3@users.noreply.github.com>
2021-04-16 22:41:25 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#sets the background color to light blue
+# sets the background color to light blue
 scene.set_background_color(9)
-#creates the player as the image of a basket and allows it to be moved by the left and right arrow keys
+# creates the player as the image of a basket and allows it to be moved by the left and right arrow keys
 Basket = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -21,7 +21,7 @@ Basket = sprites.create(img("""
     """),
     SpriteKind.player)
 controller.move_sprite(Basket, 100, 0)
-#creates list of grapes images 
+# creates list of grapes images
 list_grapes = [sprites.create(img("""
             . . . . . . . . . . . . . . . . 
                 . . . . . . . . . . . . . . . . 
@@ -79,12 +79,12 @@ list_grapes = [sprites.create(img("""
                 . . . . . . . . . . . . . . . .
         """),
         SpriteKind.food)]
-#sets the starting position of the lists of grapes, makes sure they stay within the playing screen, and sets speed
+# sets the starting position of the lists of grapes, makes sure they stay within the playing screen, and sets speed
 for index in range(3):
     list_grapes[index].set_position(80, 6)
     list_grapes[index].set_stay_in_screen(True)
     list_grapes[index].vy = 50
-#creates the acorn image which will be the players enemy
+# creates the acorn image which will be the players enemy
 Acorn = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -104,24 +104,25 @@ Acorn = sprites.create(img("""
             . . . . . . . . . . . . . . . .
     """),
     SpriteKind.enemy)
-#Sets acorns position, speed, makes sure it stays in screen and bounces off the walls
+# Sets acorns position, speed, makes sure it stays in screen and bounces off the walls
 Acorn.set_position(randint(1, 159), randint(1, 6))
 Acorn.vy = 50
 Acorn.set_stay_in_screen(True)
 Acorn.set_bounce_on_wall(True)
-#the actions under this forever will happen continuously until it is game over
+# the actions under this forever will happen continuously until it is game over
+
 def on_forever():
-# when the player touches/catches the one of the grapes in the basket they will gain 5 points and grapes will be reset in a new random position
+    # when the player touches/catches the one of the grapes in the basket they will gain 5 points and grapes will be reset in a new random position
     for index2 in range(3):
         if Basket.overlaps_with(list_grapes[index2]):
             info.change_score_by(5)
             list_grapes[index2].set_position(randint(1, 159), randint(1, 6))
-            #the player has 4 seconds to catch any one of the grapes or game over
+            # the player has 4 seconds to catch any one of the grapes or game over
             info.start_countdown(4)
         else:
-            #if the player does not catch the grape then it will continue to bounce off walls
+            # if the player does not catch the grape then it will continue to bounce off walls
             list_grapes[index2].set_bounce_on_wall(True)
-    #when the player catches the acorn it destroys the basket and it is game over 
+    # when the player catches the acorn it destroys the basket and it is game over
     if Acorn.overlaps_with(Basket):
         Basket.destroy()
         game.over(False)
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-// sets the background color to light blue
+//  sets the background color to light blue
 scene.setBackgroundColor(9)
-// creates the player as the image of a basket and allows it to be moved by the left and right arrow keys
+//  creates the player as the image of a basket and allows it to be moved by the left and right arrow keys
 let Basket = sprites.create(img`
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -20,7 +20,7 @@ let Basket = sprites.create(img`
             . . . . . . . . . . . . . . . .
     `, SpriteKind.Player)
 controller.moveSprite(Basket, 100, 0)
-// creates list of grapes images 
+//  creates list of grapes images
 let list_grapes = [sprites.create(img`
             . . . . . . . . . . . . . . . . 
                 . . . . . . . . . . . . . . . . 
@@ -73,13 +73,13 @@ let list_grapes = [sprites.create(img`
                 . . . . . 7 7 7 . . . . . . . . 
                 . . . . . . . . . . . . . . . .
         `, SpriteKind.Food)]
-// sets the starting position of the lists of grapes, makes sure they stay within the playing screen, and sets speed
+//  sets the starting position of the lists of grapes, makes sure they stay within the playing screen, and sets speed
 for (let index = 0; index < 3; index++) {
     list_grapes[index].setPosition(80, 6)
     list_grapes[index].setStayInScreen(true)
     list_grapes[index].vy = 50
 }
-// creates the acorn image which will be the players enemy
+//  creates the acorn image which will be the players enemy
 let Acorn = sprites.create(img`
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -98,27 +98,27 @@ let Acorn = sprites.create(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . .
     `, SpriteKind.Enemy)
-// Sets acorns position, speed, makes sure it stays in screen and bounces off the walls
+//  Sets acorns position, speed, makes sure it stays in screen and bounces off the walls
 Acorn.setPosition(randint(1, 159), randint(1, 6))
 Acorn.vy = 50
 Acorn.setStayInScreen(true)
 Acorn.setBounceOnWall(true)
-// the actions under this forever will happen continuously until it is game over
+//  the actions under this forever will happen continuously until it is game over
 forever(function on_forever() {
     //  when the player touches/catches the one of the grapes in the basket they will gain 5 points and grapes will be reset in a new random position
     for (let index2 = 0; index2 < 3; index2++) {
         if (Basket.overlapsWith(list_grapes[index2])) {
             info.changeScoreBy(5)
             list_grapes[index2].setPosition(randint(1, 159), randint(1, 6))
-            // the player has 4 seconds to catch any one of the grapes or game over
+            //  the player has 4 seconds to catch any one of the grapes or game over
             info.startCountdown(4)
         } else {
-            // if the player does not catch the grape then it will continue to bounce off walls
+            //  if the player does not catch the grape then it will continue to bounce off walls
             list_grapes[index2].setBounceOnWall(true)
         }
         
     }
-    // when the player catches the acorn it destroys the basket and it is game over 
+    //  when the player catches the acorn it destroys the basket and it is game over
     if (Acorn.overlapsWith(Basket)) {
         Basket.destroy()
         game.over(false)
