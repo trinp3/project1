0293615539381f981a4e04d6e546923fe1dcab59
trinp3 <81163831+trinp3@users.noreply.github.com>
2021-04-16 21:54:56 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,66 +1,6 @@
-def on_a_pressed():
-    global list_grapes
-    list_grapes = [sprites.create(img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . 4 e . . . . . . . . 
-                        . . . . . . 4 e . . . . . . . . 
-                        . . . . . . e 4 . . . . . . . . 
-                        . . . . c c c e . . . . . . . . 
-                        . . . . a a c c c c . . . . . . 
-                        . . . . a a c c c c c . . . . . 
-                        . . . . . c a a c c c . . . . . 
-                        . . . . c c a a c c . . . . . . 
-                        . . . . c a c c c c . . . . . . 
-                        . . . . . a a c c . . . . . . . 
-                        . . . . c c a a c c . . . . . . 
-                        . . . . c c c a a c . . . . . . 
-                        . . . . . c a c . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            SpriteKind.food),
-        sprites.create(img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . 4 e . . . . . . . . 
-                        . . . . . . 4 e . . . . . . . . 
-                        . . . . . . e 4 . . . . . . . . 
-                        . . . . 2 2 2 e . . . . . . . . 
-                        . . . . 2 2 2 2 2 2 . . . . . . 
-                        . . . . 2 2 2 2 2 2 2 . . . . . 
-                        . . . . . 2 2 2 2 2 2 . . . . . 
-                        . . . . 2 2 2 2 2 2 . . . . . . 
-                        . . . . 2 2 2 2 2 2 . . . . . . 
-                        . . . . . 2 2 2 2 . . . . . . . 
-                        . . . . 2 2 2 2 2 2 . . . . . . 
-                        . . . . 2 2 2 2 2 2 . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            SpriteKind.food),
-        sprites.create(img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . 4 e . . . . . . . . 
-                        . . . . . . 4 e . . . . . . . . 
-                        . . . . . . e 4 . . . . . . . . 
-                        . . . . 7 7 7 e . . . . . . . . 
-                        . . . . 7 7 7 7 7 7 . . . . . . 
-                        . . . . 7 7 7 7 7 7 7 . . . . . 
-                        . . . . . 7 7 7 7 7 7 . . . . . 
-                        . . . . 7 7 7 7 7 7 . . . . . . 
-                        . . . . 7 7 7 7 7 7 . . . . . . 
-                        . . . . . 7 7 7 7 . . . . . . . 
-                        . . . . 7 7 7 7 7 7 . . . . . . 
-                        . . . . 7 7 7 7 7 7 . . . . . . 
-                        . . . . . 7 7 7 . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            SpriteKind.food)]
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
-
-list_grapes: List[Sprite] = []
+#sets the background color to light blue
 scene.set_background_color(9)
+#creates the player as the image of a basket and allows it to be moved by the left and right arrow keys
 Basket = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -81,28 +21,70 @@ Basket = sprites.create(img("""
     """),
     SpriteKind.player)
 controller.move_sprite(Basket, 100, 0)
-Grapes = sprites.create(img("""
-        . . . . . . . . . . . . . . . . 
+#creates list of grapes images 
+list_grapes = [sprites.create(img("""
             . . . . . . . . . . . . . . . . 
-            . . . . . . 4 e . . . . . . . . 
-            . . . . . . 4 e . . . . . . . . 
-            . . . . . . e 4 . . . . . . . . 
-            . . . . c c c e . . . . . . . . 
-            . . . . a a c c c c . . . . . . 
-            . . . . a a c c c c c . . . . . 
-            . . . . . c a a c c c . . . . . 
-            . . . . c c a a c c . . . . . . 
-            . . . . c a c c c c . . . . . . 
-            . . . . . a a c c . . . . . . . 
-            . . . . c c a a c c . . . . . . 
-            . . . . c c c a a c . . . . . . 
-            . . . . . c a c . . . . . . . . 
-            . . . . . . . . . . . . . . . .
-    """),
-    SpriteKind.food)
-Grapes.set_position(80, 6)
-Grapes.set_stay_in_screen(True)
-Grapes.vy = 50
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . e 4 . . . . . . . . 
+                . . . . c c c e . . . . . . . . 
+                . . . . a a c c c c . . . . . . 
+                . . . . a a c c c c c . . . . . 
+                . . . . . c a a c c c . . . . . 
+                . . . . c c a a c c . . . . . . 
+                . . . . c a c c c c . . . . . . 
+                . . . . . a a c c . . . . . . . 
+                . . . . c c a a c c . . . . . . 
+                . . . . c c c a a c . . . . . . 
+                . . . . . c a c . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.food),
+    sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . e 4 . . . . . . . . 
+                . . . . 2 2 2 e . . . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 2 . . . . . 
+                . . . . . 2 2 2 2 2 2 . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . . 2 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.food),
+    sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . e 4 . . . . . . . . 
+                . . . . 7 7 7 e . . . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . 7 7 7 7 7 7 7 . . . . . 
+                . . . . . 7 7 7 7 7 7 . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . . 7 7 7 7 . . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . . 7 7 7 . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.food)]
+#sets the starting position of the lists of grapes, makes sure they stay within the playing screen, and sets speed
+for index in range(3):
+    list_grapes[index].set_position(80, 6)
+    list_grapes[index].set_stay_in_screen(True)
+    list_grapes[index].vy = 50
+#creates the acorn image which will be the players enemy
 Acorn = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -122,18 +104,24 @@ Acorn = sprites.create(img("""
             . . . . . . . . . . . . . . . .
     """),
     SpriteKind.enemy)
+#Sets acorns position, speed, makes sure it stays in screen and bounces off the walls
 Acorn.set_position(randint(1, 159), randint(1, 6))
 Acorn.vy = 50
 Acorn.set_stay_in_screen(True)
 Acorn.set_bounce_on_wall(True)
-
+#the actions under this forever will happen continuously until it is game over
 def on_forever():
-    if Basket.overlaps_with(Grapes):
-        info.change_score_by(5)
-        Grapes.set_position(randint(1, 159), randint(1, 6))
-        info.start_countdown(5)
-    else:
-        Grapes.set_bounce_on_wall(True)
+# when the player touches/catches the one of the grapes in the basket they will gain 5 points and grapes will be reset in a new random position
+    for index2 in range(3):
+        if Basket.overlaps_with(list_grapes[index2]):
+            info.change_score_by(5)
+            list_grapes[index2].set_position(randint(1, 159), randint(1, 6))
+            #the player has 4 seconds to catch any one of the grapes or game over
+            info.start_countdown(4)
+        else:
+            #if the player does not catch the grape then it will continue to bounce off walls
+            list_grapes[index2].set_bounce_on_wall(True)
+    #when the player catches the acorn it destroys the basket and it is game over 
     if Acorn.overlaps_with(Basket):
         Basket.destroy()
         game.over(False)
--- main.ts
+++ main.ts
@@ -1,114 +1,127 @@
+// sets the background color to light blue
 scene.setBackgroundColor(9)
+// creates the player as the image of a basket and allows it to be moved by the left and right arrow keys
 let Basket = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . 4 e e 4 e e 4 e e 4 e e . . 
-    . . e . . . . . . . . . . 4 . . 
-    . . 4 . . . . . . . . . . e . . 
-    . . e e e 4 4 4 e e e 4 4 4 . . 
-    . . e e e 4 4 4 e e e 4 4 4 . . 
-    . . e e e 4 4 4 e e e 4 4 4 . . 
-    . . 4 4 4 e e e 4 4 4 e e e . . 
-    . . 4 4 4 e e e 4 4 4 e e e . . 
-    . . 4 4 4 e e e 4 4 4 e e e . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . 4 e e 4 e e 4 e e 4 e e . . 
+            . . e . . . . . . . . . . 4 . . 
+            . . 4 . . . . . . . . . . e . . 
+            . . e e e 4 4 4 e e e 4 4 4 . . 
+            . . e e e 4 4 4 e e e 4 4 4 . . 
+            . . e e e 4 4 4 e e e 4 4 4 . . 
+            . . 4 4 4 e e e 4 4 4 e e e . . 
+            . . 4 4 4 e e e 4 4 4 e e e . . 
+            . . 4 4 4 e e e 4 4 4 e e e . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
     `, SpriteKind.Player)
 controller.moveSprite(Basket, 100, 0)
+// creates list of grapes images 
 let list_grapes = [sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 4 e . . . . . . . . 
-    . . . . . . 4 e . . . . . . . . 
-    . . . . . . e 4 . . . . . . . . 
-    . . . . c c c e . . . . . . . . 
-    . . . . a a c c c c . . . . . . 
-    . . . . a a c c c c c . . . . . 
-    . . . . . c a a c c c . . . . . 
-    . . . . c c a a c c . . . . . . 
-    . . . . c a c c c c . . . . . . 
-    . . . . . a a c c . . . . . . . 
-    . . . . c c a a c c . . . . . . 
-    . . . . c c c a a c . . . . . . 
-    . . . . . c a c . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Food), sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 4 e . . . . . . . . 
-    . . . . . . 4 e . . . . . . . . 
-    . . . . . . e 4 . . . . . . . . 
-    . . . . 2 2 2 e . . . . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . . . 
-    . . . . 2 2 2 2 2 2 2 . . . . . 
-    . . . . . 2 2 2 2 2 2 . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . . . 
-    . . . . . 2 2 2 2 . . . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . . . 
-    . . . . . 2 2 2 . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Food), sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 4 e . . . . . . . . 
-    . . . . . . 4 e . . . . . . . . 
-    . . . . . . e 4 . . . . . . . . 
-    . . . . 7 7 7 e . . . . . . . . 
-    . . . . 7 7 7 7 7 7 . . . . . . 
-    . . . . 7 7 7 7 7 7 7 . . . . . 
-    . . . . . 7 7 7 7 7 7 . . . . . 
-    . . . . 7 7 7 7 7 7 . . . . . . 
-    . . . . 7 7 7 7 7 7 . . . . . . 
-    . . . . . 7 7 7 7 . . . . . . . 
-    . . . . 7 7 7 7 7 7 . . . . . . 
-    . . . . 7 7 7 7 7 7 . . . . . . 
-    . . . . . 7 7 7 . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Food)]
-for (let index = 0; index <= 2; index++) {
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . e 4 . . . . . . . . 
+                . . . . c c c e . . . . . . . . 
+                . . . . a a c c c c . . . . . . 
+                . . . . a a c c c c c . . . . . 
+                . . . . . c a a c c c . . . . . 
+                . . . . c c a a c c . . . . . . 
+                . . . . c a c c c c . . . . . . 
+                . . . . . a a c c . . . . . . . 
+                . . . . c c a a c c . . . . . . 
+                . . . . c c c a a c . . . . . . 
+                . . . . . c a c . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        `, SpriteKind.Food), sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . e 4 . . . . . . . . 
+                . . . . 2 2 2 e . . . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 2 . . . . . 
+                . . . . . 2 2 2 2 2 2 . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . . 2 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        `, SpriteKind.Food), sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . 4 e . . . . . . . . 
+                . . . . . . e 4 . . . . . . . . 
+                . . . . 7 7 7 e . . . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . 7 7 7 7 7 7 7 . . . . . 
+                . . . . . 7 7 7 7 7 7 . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . . 7 7 7 7 . . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . 7 7 7 7 7 7 . . . . . . 
+                . . . . . 7 7 7 . . . . . . . . 
+                . . . . . . . . . . . . . . . .
+        `, SpriteKind.Food)]
+// sets the starting position of the lists of grapes, makes sure they stay within the playing screen, and sets speed
+for (let index = 0; index < 3; index++) {
     list_grapes[index].setPosition(80, 6)
     list_grapes[index].setStayInScreen(true)
     list_grapes[index].vy = 50
 }
+// creates the acorn image which will be the players enemy
 let Acorn = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . e . . . . . . . . 
-    . . . . . . . e . . . . . . . . 
-    . . . . . 4 e 4 e 4 . . . . . . 
-    . . . . . e 4 e 4 e . . . . . . 
-    . . . . . . e e e . . . . . . . 
-    . . . . . . e e e . . . . . . . 
-    . . . . . . . e . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . e . . . . . . . . 
+            . . . . . . . e . . . . . . . . 
+            . . . . . 4 e 4 e 4 . . . . . . 
+            . . . . . e 4 e 4 e . . . . . . 
+            . . . . . . e e e . . . . . . . 
+            . . . . . . e e e . . . . . . . 
+            . . . . . . . e . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
     `, SpriteKind.Enemy)
+// Sets acorns position, speed, makes sure it stays in screen and bounces off the walls
 Acorn.setPosition(randint(1, 159), randint(1, 6))
 Acorn.vy = 50
 Acorn.setStayInScreen(true)
 Acorn.setBounceOnWall(true)
-forever(function () {
-    for (let index = 0; index <= 2; index++) {
-        if (Basket.overlapsWith(list_grapes[index])) {
+// the actions under this forever will happen continuously until it is game over
+forever(function on_forever() {
+    //  when the player touches/catches the one of the grapes in the basket they will gain 5 points and grapes will be reset in a new random position
+    for (let index2 = 0; index2 < 3; index2++) {
+        if (Basket.overlapsWith(list_grapes[index2])) {
             info.changeScoreBy(5)
-            list_grapes[index].setPosition(randint(1, 159), randint(1, 6))
+            list_grapes[index2].setPosition(randint(1, 159), randint(1, 6))
+            // the player has 4 seconds to catch any one of the grapes or game over
             info.startCountdown(4)
         } else {
-            list_grapes[index].setBounceOnWall(true)
+            // if the player does not catch the grape then it will continue to bounce off walls
+            list_grapes[index2].setBounceOnWall(true)
         }
+        
     }
+    // when the player catches the acorn it destroys the basket and it is game over 
     if (Acorn.overlapsWith(Basket)) {
         Basket.destroy()
         game.over(false)
     }
+    
 })
